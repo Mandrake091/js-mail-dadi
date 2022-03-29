9a5f1674bd5b344143fffd6176f518bd93a9277c
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-03-29 16:50:18 +0200

    added: user + cpu results

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Test di benvenuto
 const h1 = document.createElement('h1');
-const textH1 = document.createTextNode("Benvenuto in questa pagina!");
+const textH1 = document.createTextNode("Benvenutæ in questa pagina!");
 h1.appendChild(textH1);
 
 //Bottone benvenuto
@@ -19,10 +19,14 @@ col.setAttribute('class', 'col-12 text-center');
 col.append(h1);
 col.append(button);
 
+
+
+
 //Riga 
 const row = document.createElement('div');
 row.setAttribute('class', 'row');
 row.append(col);
+
 
 
 //Container
@@ -34,13 +38,9 @@ container.append(row);
 const app = document.getElementById('app');
 app.append(container);
 
+
 //Lista di persone che possono accedere
-
-let accessDenied = false;
-
 const emailList = ["boolean@careers.it", "paolo.cannistraro@live.it", "pincopallo@gmail.com", "classe59@gmail.com", "javascript@js.com"];
-
-
 
 console.log(emailList)
 
@@ -57,20 +57,80 @@ function getInputValue() {
 
     const inputValue = document.getElementById("input").value;
 
-
-
     if (!emailList.includes(inputValue)) {
-        
+
         shake.classList.add('error');
 
     } else {
         shake.classList.remove('error');
         const h1 = document.createElement('h1');
-        const textH1 = document.createTextNode("Benvenuto " + inputValue);
+        const textH1 = document.createTextNode("Benvenutæ: " + inputValue);
         h1.appendChild(textH1);
         email.append(h1);
     }
 }
+
+
+
+
+
+let user = 1 + Math.floor(Math.random() * 6);
+let cpu = 1 + Math.floor(Math.random() * 6);
+
+
+
+
+
+
+
+for (let i = 0; i < 1; i++) {
+
+    if (user > cpu) {
+        console.log("l'utente ha vinto!")
+    } else {
+
+        console.log("la cpu ha vinto!")
+    }
+
+    console.log(user + "user")
+    console.log(cpu + "cpu")
+}
+
+
+//Format per la visualizzazione dei punteggi
+
+//User
+const colGameUser = document.createElement('div');
+const h1User = document.createElement('h1');
+const userResult = document.createTextNode("User");
+h1User.appendChild(userResult);
+colGameUser.append(h1User);
+colGameUser.setAttribute('class', 'col-6 text-center d-none');
+const pUser = document.createElement('p').innerHTML=user;
+
+colGameUser.append(pUser);
+
+//Cpu
+
+const colGameCpu = document.createElement('div');
+const h1Cpu = document.createElement('h1');
+const cpuResult = document.createTextNode("Cpu");
+h1Cpu.appendChild(cpuResult);
+colGameCpu.append(h1Cpu);
+colGameCpu.setAttribute('class', 'col-6 text-center d-none');
+const pCpu = document.createElement('p').innerHTML=cpu;
+
+colGameCpu.append(pCpu);
+
+
+row.append(colGameUser);
+row.append(colGameCpu);
+
+
+
+
+
+
 
 
 
@@ -84,5 +144,7 @@ document.getElementById("btn").addEventListener("click", display);
 
 function display() {
     col.classList.add("d-none");
-    email.classList.remove("d-none")
+    email.classList.remove("d-none");
+    colGameCpu.classList.remove("d-none");
+    colGameUser.classList.remove("d-none");
 }
